584ebeda73df0459ffa37abad743bd8a9c31172d
Sebastian Fiorini <sebf465@gmail.com>
2016-12-15 21:58:23 -0500

    Probably best like this for now

--- .setup.sh
+++ .setup.sh
@@ -1,7 +1,7 @@
 # Setup script when going around to new computers
 
-# Best tools
-sudo apt-get install git mercurial vim zsh clipit
+# Best tools 
+sudo apt-get install git tmux vim zsh clipit
 
 # the tool itself has a NSFW name, pass it in as an option to install
 if [ "$1" == fuck]; then
@@ -19,7 +19,7 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 git config --global user.name "Sebastian Fiorini"
 git config --global user.email "sebf465@gmail.com"
 
-# Get dotfiles from the interwebz and place them in home
+# Get dotfiles from the web and place them in home
 git clone https://github.com/sebfio/dotfiles
 mv dotfiles/.* $HOME 
 
